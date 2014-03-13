8213587d9ee22eea796caa88da69059e7728634a
Teresa <teresa.nuzzo@gmail.com>
2014-03-13 10:19:05 -0400

    removed use of l in migration

--- db/migrate/001_add_project_priority.rb
+++ db/migrate/001_add_project_priority.rb
@@ -4,10 +4,10 @@ class AddProjectPriority < ActiveRecord::Migration
     add_index "projects", ["priority_id"], :name => "index_projects_on_priority_id"
     
     # Same default values as for IssuePriority
-    ProjectPriority.create!(:name => l(:default_priority_low), :position => 1)
-    ProjectPriority.create!(:name => l(:default_priority_normal), :position => 2, :is_default => true)
-    ProjectPriority.create!(:name => l(:default_priority_high), :position => 3)
-    ProjectPriority.create!(:name => l(:default_priority_urgent), :position => 4)
-    ProjectPriority.create!(:name => l(:default_priority_immediate), :position => 5)
+    ProjectPriority.create!(:name => :default_priority_low, :position => 1)
+    ProjectPriority.create!(:name => :default_priority_normal, :position => 2, :is_default => true)
+    ProjectPriority.create!(:name => :default_priority_high, :position => 3)
+    ProjectPriority.create!(:name => :default_priority_urgent, :position => 4)
+    ProjectPriority.create!(:name => :default_priority_immediate, :position => 5)
   end
 end
