d0db252002724363b0b63f59d8ebe39251708f9d
Teresa <teresa.nuzzo@gmail.com>
2014-03-13 10:26:46 -0400

    migration error checking

--- db/migrate/001_add_project_priority.rb
+++ db/migrate/001_add_project_priority.rb
@@ -1,7 +1,16 @@
 class AddProjectPriority < ActiveRecord::Migration
   def change
-    add_column :projects, :priority_id, :integer
-    add_index "projects", ["priority_id"], :name => "index_projects_on_priority_id"
+    begin
+      add_column :projects, :priority_id, :integer
+    rescue => ex
+      say ex
+    end
+    
+    begin
+      add_index "projects", ["priority_id"], :name => "index_projects_on_priority_id"
+    rescue => ex
+      say ex
+    end
     
     # Same default values as for IssuePriority
     ProjectPriority.create!(:name => :default_priority_low, :position => 1)
