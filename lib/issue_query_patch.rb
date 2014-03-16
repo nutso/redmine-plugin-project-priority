cb1b5ff870fd174aef51d41de2b3434766b205b3
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 15:33:28 -0400

    priority instead of id

--- lib/issue_query_patch.rb
+++ lib/issue_query_patch.rb
@@ -11,7 +11,7 @@ module RedmineProjectPriority
         alias_method_chain :initialize_available_filters, :project_priority_filter
         
         # inherit from IssueQuery, just add project priority
-        self.available_columns << QueryColumn.new(:project_priority_id, :sortable => "#{ProjectPriority.table_name}.position", :default_order => 'desc', :groupable => true, :caption => :label_project_priority)
+        self.available_columns << QueryColumn.new(:project_priority, :sortable => "#{ProjectPriority.table_name}.position", :default_order => 'desc', :groupable => true, :caption => :label_project_priority)
       end # base.class_eval
     end # self.included
   end # issues patch
