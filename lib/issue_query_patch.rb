0cf4476048019421252650057d4c09f7fac6ffb6
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 13:00:17 -0400

    code formatting

--- lib/issue_query_patch.rb
+++ lib/issue_query_patch.rb
@@ -2,11 +2,8 @@ module RedmineProjectPriority
   module IssueQueryPatch
     def self.included(base)
       base.class_eval do
-
-
         # inherit from IssueQuery, just add project priority
         self.available_columns << QueryColumn.new(:project_priority, :sortable => "#{ProjectPriority.table_name}.position", :default_order => 'desc', :groupable => true, :caption => :label_project_priority)
-
       end # base.class_eval
     end # self.included
   end # issues patch
