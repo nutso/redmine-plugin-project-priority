3e9566796941b064b49a3fa8638d4bc850b94c12
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 13:56:05 -0400

    using alias chain instead of controller filter in model

--- lib/issue_query_patch.rb
+++ lib/issue_query_patch.rb
@@ -2,14 +2,20 @@ module RedmineProjectPriority
   module IssueQueryPatch
     def self.included(base)
       base.class_eval do
+        alias_method_chain :initialize_available_filters, :project_priority_filter
+        
         after_filter :add_project_priority_filter, :initialize_available_filters
         
         # inherit from IssueQuery, just add project priority
         self.available_columns << QueryColumn.new(:project_priority, :sortable => "#{ProjectPriority.table_name}.position", :default_order => 'desc', :groupable => true, :caption => :label_project_priority)
         
-        def add_project_priority_filter
+        def initialize_available_filters_with_project_priority_filter
+          reval = initialize_available_filters_without_project_priority_filter
+          
           add_available_filter "project_priority_id",
             :type => :list, :values => ProjectPriority.all.collect{|s| [s.name, s.id.to_s] }
+            
+          retval
         end
         
         # TODO add_available_filter
