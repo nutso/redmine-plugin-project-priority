65a7aaaeae4e1fc88a243ecf0e5a15dff3247e0f
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 15:37:52 -0400

    project

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -4,7 +4,7 @@ module RedmineProjectPriority
       base.class_eval do
         # making the project priority available directly as an issue attribute
         # used for view queries and filters
-        has_one :project_priority, :through => :projects, :source => :project_priority
+        has_one :project_priority, :through => :project, :source => :project_priority
       end # base.class_eval
     end # self.included
   end # issues patch
