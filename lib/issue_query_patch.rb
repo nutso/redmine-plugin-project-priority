5e0b50b483d2b6f4de31f20d60bbd8c6c7566197
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 14:00:35 -0400

    seeing if location in file is important

--- lib/issue_query_patch.rb
+++ lib/issue_query_patch.rb
@@ -1,19 +1,18 @@
 module RedmineProjectPriority
   module IssueQueryPatch
     def self.included(base)
+      def initialize_available_filters_with_project_priority_filter
+        reval = initialize_available_filters_without_project_priority_filter
+        add_available_filter "project_priority_id", :type => :list, :values => ProjectPriority.all.collect{|s| [s.name, s.id.to_s] }
+        retval
+      end
+
       base.class_eval do
         alias_method_chain :initialize_available_filters, :project_priority_filter
         
         # inherit from IssueQuery, just add project priority
         self.available_columns << QueryColumn.new(:project_priority, :sortable => "#{ProjectPriority.table_name}.position", :default_order => 'desc', :groupable => true, :caption => :label_project_priority)
       end # base.class_eval
-      
-      def initialize_available_filters_with_project_priority_filter
-        reval = initialize_available_filters_without_project_priority_filter
-        add_available_filter "project_priority_id", :type => :list, :values => ProjectPriority.all.collect{|s| [s.name, s.id.to_s] }
-        retval
-      end
-      
     end # self.included
   end # issues patch
 end # recurring task
