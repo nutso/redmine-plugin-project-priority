35a490e22df528278e8a9ee171c182ad1f3fbc8a
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 12:43:25 -0400

    just defining the attribute right out instead of trying to alias it

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -3,7 +3,13 @@ module RedmineProjectPriority
     def self.included(base)
       base.class_eval do
         # making the project priority available directly as an issue attribute
-        alias_attribute :project_priority, :project.priority 
+        # alias_attribute :project_priority, :project.priority
+        def project_priority
+          project.priority
+        end 
+        def project_priority=(incoming)
+          project.priority=incoming
+        end
       end # base.class_eval
     end # self.included
   end # issues patch
