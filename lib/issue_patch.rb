7d06091005c7628e9a5462a178f52c9ac84dc296
Teresa <teresa.nuzzo@gmail.com>
2014-03-16 13:06:58 -0400

    exposing project priority id rather than just the priority

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -3,13 +3,10 @@ module RedmineProjectPriority
     def self.included(base)
       base.class_eval do
         # making the project priority available directly as an issue attribute
-        # alias_attribute :project_priority, :project.priority
-        def project_priority
-          project.priority
+        # used for view queries and filters
+        def project_priority_id
+          project.priority.id
         end 
-        def project_priority=(incoming)
-          project.priority=incoming
-        end
       end # base.class_eval
     end # self.included
   end # issues patch
